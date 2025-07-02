48d29e63a1d1c5b47574f5e5f192f8cfe8086027
Darlan Alvese <darlanalp@gmail.comm>
2025-07-01 23:28:53 -0300

    Trabalhando com templates, if, else e for

--- gerenciador-usuarios/src/app/app.ts
+++ gerenciador-usuarios/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { signalGetFn } from '@angular/core/primitives/signals';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root',  
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'gerenciador-usuarios';
+ shwow = signal(true);
+ users = signal(['Darlan','José', 'Maria']);
 }
