f767fa6881b018796ef603b6513e1f85fdb9b420
Darlan Alvese <darlanalp@gmail.comm>
2025-07-02 23:30:36 -0300

    Revisando criação componentes standalone API e Signal

--- gerenciador-usuarios/src/app/app.ts
+++ gerenciador-usuarios/src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { signalGetFn } from '@angular/core/primitives/signals';
-import { RouterOutlet } from '@angular/router';
+import { UsersList } from './list/components/users-list/users-list';
 
 @Component({
   selector: 'app-root',  
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [UsersList]
 })
+
 export class App {
- shwow = signal(true);
- users = signal(['Darlan','José', 'Maria']);
+
+  remove(user: string) {
+    this.users.update(usersX => usersX.filter(u => u !== user));
+  }
+
+  users = signal(['Darlan','José', 'Maria', 'Carlão']);
 }
