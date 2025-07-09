a5d5fecefc38abc100120f1d1b2513eeb7d8089a
Darlan Alves <darlan.alves@totvs.com.br>
2025-07-08 21:08:02 -0300

    exemplos usando o conceito de "model", não precisa criar um input e output, o model faz o papel dos dois

--- gerenciador-usuarios/src/app/app.ts
+++ gerenciador-usuarios/src/app/app.ts
@@ -1,14 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { UsersList } from './list/components/users-list/users-list';
+import { SearchInput } from "./list/components/search-input/search-input";
 
 @Component({
   selector: 'app-root',  
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [UsersList]
+  imports: [UsersList, SearchInput]
 })
 
 export class App {
+
+  search = signal('');
+  
+  filteredUsers = computed(() => {
+     return this.users().filter(user => user.toLowerCase().includes(this.search().toLowerCase()))
+    }
+  );
 
   remove(user: string) {
     this.users.update(usersX => usersX.filter(u => u !== user));
--- gerenciador-usuarios/src/app/list/components/users-list/users-list.ts
+++ gerenciador-usuarios/src/app/list/components/users-list/users-list.ts
@@ -2,24 +2,16 @@ import { Component, computed, input, output,  signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-users-list',
-  imports: [FormsModule],
+  selector: 'app-users-list', 
   templateUrl: './users-list.html',
   styleUrl: './users-list.scss'
 })
 
 export class UsersList {
 
-  filteredUsers = computed(() => {
-     return this.users().filter(user => user.toLowerCase().includes(this.search().toLowerCase()))
-    }
-  );
-
   users = input.required<string[]>();
   removeUser = output<string>({alias:'remove'});
   
-  search = signal('');
-
   remove(user: string) {
     this.removeUser.emit(user);
   }
